2bfdc3854ae3981b57110d4227c090a84703ead1
Prabrisha Das <prabrishadas05@gmail.com>
2025-07-20 15:34:50 +0530

    Update assignment1_3.py

--- assignment_1/assignment1_3.py
+++ assignment_1/assignment1_3.py
@@ -1,5 +1,5 @@
 # 3. Write a program to take a 2-digit number and then print the reversed number. That is, if the input given is 25, the program should print 52. Then extend it for 3-digit number and also check for odd and even
 
-num = int(input("Enter a 2 or 3-digit number: ")) 
+num = (input("Enter a 2 or 3-digit number: ")
 print(f"Reversed: {(num)[::-1]}")
 print(f"{num} is {'Even' if int(num) % 2 == 0 else 'Odd'}")
